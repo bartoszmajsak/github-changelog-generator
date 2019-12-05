cc29381288564006f65d615967ac326403a7773d
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-12-05 21:20:11 +0100

    feat: introduces adoc template

--- pkg/cmd/generate/templates.go
+++ pkg/cmd/generate/templates.go
@@ -8,9 +8,7 @@ type Changelog struct {
 }
 
 const Default = `
-# Highlights of {{.Release }} release
-
-## New features
+### New features
 
 {{- with $prs := (index .PullRequests "enhancement") -}}
 {{range $pr := $prs }}
@@ -18,7 +16,7 @@ const Default = `
 {{- end -}}
 {{ end }}
 
-## Bug fixes
+### Bug fixes
 
 {{- with $prs := (index .PullRequests "bug") -}}
 {{range $pr := $prs }}
@@ -26,7 +24,7 @@ const Default = `
 {{- end -}}
 {{ end }}
 
-## Dependencies update
+### Dependencies update
 
 {{- with $prs := (index .PullRequests "dependencies") -}}
 {{range $pr := $prs }}
@@ -34,7 +32,7 @@ const Default = `
 {{- end -}}
 {{ end }}
 
-## Project infrastructure
+### Project infrastructure
 
 {{- with $prs := (index .PullRequests "infra") -}}
 {{range $pr := $prs }}
@@ -42,9 +40,67 @@ const Default = `
 {{- end -}}
 {{ end }}
 
-## Testing
+### Testing
 
 {{- with $prs := (index .PullRequests "test-infra") -}}
+{{range $pr := $prs }}
+ * {{$pr.Title}} ([#{{$pr.Number}}]({{$pr.Permalink}})), by [@{{$pr.Author}}](https://github.com/{{$pr.Author}})
+{{- end -}}
+{{ end }}
+
+### Misc
+
+{{- with $prs := (index .PullRequests "uncategorized") -}}
+{{range $pr := $prs }}
+ * {{$pr.Title}} ([#{{$pr.Number}}]({{$pr.Permalink}})), by [@{{$pr.Author}}](https://github.com/{{$pr.Author}})
+{{- end -}}
+{{ end }}
+`
+
+const DefaultAdoc = `
+=== New features
+
+{{- with $prs := (index .PullRequests "enhancement") -}}
+{{range $pr := $prs }}
+ * {{$pr.Title}} ({{$pr.Permalink}}[#{{$pr.Number}}]), by https://github.com/{{$pr.Author}}[@{{$pr.Author}}]
+{{- end -}}
+{{ end }}
+
+=== Bug fixes
+
+{{- with $prs := (index .PullRequests "bug") -}}
+{{range $pr := $prs }}
+ * {{$pr.Title}} ({{$pr.Permalink}}[#{{$pr.Number}}]), by https://github.com/{{$pr.Author}}[@{{$pr.Author}}]
+{{- end -}}
+{{ end }}
+
+=== Dependencies update
+
+{{- with $prs := (index .PullRequests "dependencies") -}}
+{{range $pr := $prs }}
+ * {{$pr.Title}} ({{$pr.Permalink}}[#{{$pr.Number}}])
+{{- end -}}
+{{ end }}
+
+=== Project infrastructure
+
+{{- with $prs := (index .PullRequests "infra") -}}
+{{range $pr := $prs }}
+ * {{$pr.Title}} ({{$pr.Permalink}}[#{{$pr.Number}}]), by https://github.com/{{$pr.Author}}[@{{$pr.Author}}]
+{{- end -}}
+{{ end }}
+
+=== Testing
+
+{{- with $prs := (index .PullRequests "test-infra") -}}
+{{range $pr := $prs }}
+ * {{$pr.Title}} ({{$pr.Permalink}}[#{{$pr.Number}}]), by https://github.com/{{$pr.Author}}[@{{$pr.Author}}]
+{{- end -}}
+{{ end }}
+
+=== Misc
+
+{{- with $prs := (index .PullRequests "uncategorized") -}}
 {{range $pr := $prs }}
  * {{$pr.Title}} ([#{{$pr.Number}}]({{$pr.Permalink}})), by [@{{$pr.Author}}](https://github.com/{{$pr.Author}})
 {{- end -}}
