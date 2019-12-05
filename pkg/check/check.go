190d02f2ea573e666c637be1ecf7630e5a2372ab
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-12-05 16:20:01 +0100

    chore: exposes red format for printing out msgs

--- pkg/check/check.go
+++ pkg/check/check.go
@@ -6,19 +6,22 @@ import (
 	"strings"
 )
 
+const RedFmt = "\x1b[31;1m%s\x1b[0m\n"
+
 // IfError checks if error occurred, logs it and exits
 func IfError(err error) {
 	if err == nil {
 		return
 	}
-	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
+	fmt.Printf(RedFmt, fmt.Sprintf("error: %s", err))
 	os.Exit(1)
 }
 
+// RepoFormat checks if repository string conforms with expected format, i.e. owner/repo
 func RepoFormat(repo string) {
 	repoParts := strings.Split(repo, "/")
 	if len(repoParts) != 2 {
-		fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("wrong repo format: %s. please make sure it's in owner/repo format", repo))
+		fmt.Printf(RedFmt, fmt.Sprintf("wrong repo format: %s. please make sure it's in owner/repo format", repo))
 		os.Exit(1)
 	}
 }
