86bec42d4434dcf82ddf83ac38153f97b979b97a
Bartosz Majsak <bartosz.majsak@gmail.com>
2020-04-15 08:48:08 +0200

    fix: removes dead code

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,10 +5,6 @@ import (
 	"path"
 	"strings"
 
-	"github.com/hashicorp/go-multierror"
-
-	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
@@ -66,45 +62,4 @@ func contains(s []string, e string) bool {
 		}
 	}
 	return false
-}
-
-// SyncFlag ensures that if configuration provides a value for a given cmd.flag it will be set back to the flag itself,
-// but only if the flag was not set through CLI.
-//
-// This way we can make flags required but still have their values provided by the configuration source
-func SyncFlag(cmd *cobra.Command, flagName string) error { //nolint[:unused]
-	value := viper.GetString(cmd.Name() + "." + flagName)
-	if value != "" && !cmd.Flag(flagName).Changed {
-		return cmd.Flags().Set(flagName, value)
-	}
-	return nil
-}
-
-// SyncFlags ensures that if configuration provide a value for any of defined flags it will be set
-// back to the flag itself.
-//
-// This function iterates over all flags defined for cobra.Command and accumulates errors if they occur while
-// calling SyncFlag for every flag.
-func SyncFlags(cmd *cobra.Command) error { //nolint[:unused]
-	var accErrors *multierror.Error
-	cmd.Flags().VisitAll(func(flag *pflag.Flag) {
-		syncFlagErr := SyncFlag(cmd, flag.Name)
-		accErrors = multierror.Append(accErrors, syncFlagErr)
-	})
-	return accErrors.ErrorOrNil()
-}
-
-// BindFullyQualifiedFlag ensures that each flag used in commands is bound to a key using fully qualified name
-// which has a following form:
-//
-// 		commandName.flagName
-//
-// This lets us  keep structure of yaml file:
-//
-//	commandName:
-//		flagName: value
-func BindFullyQualifiedFlag(cmd *cobra.Command) func(flag *pflag.Flag) { //nolint[:unused]
-	return func(flag *pflag.Flag) {
-		_ = viper.BindPFlag(cmd.Name()+"."+flag.Name, flag)
-	}
 }
